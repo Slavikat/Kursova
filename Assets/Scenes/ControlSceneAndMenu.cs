b36d7210e34785e7c1e1253dfa3a922c79628834
Misha Medyk <misha.medyk2000@gmail.com>
2019-12-17 18:51:27 +0200

    Menu

---
+++ Assets/Scenes/ControlSceneAndMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlSceneAndMenu : MonoBehaviour
+{
+    public GameObject buttonsMenu;
+    public GameObject buttonsExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowExitButtons()
+    {
+        buttonsMenu.SetActive(false);
+        buttonsExit.SetActive(true);
+    }
+
+    public void BackInMenu()
+    {
+        buttonsMenu.SetActive(true);
+        buttonsExit.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
