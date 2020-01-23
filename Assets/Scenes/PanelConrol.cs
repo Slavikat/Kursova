2bb8f2af2fe1fcf0cf7a84be41cde668c00e15db
IrynaJuriivna <45572834+IrynaJuriivna@users.noreply.github.com>
2020-01-23 23:54:51 +0200

    Ready

---
+++ Assets/Scenes/PanelConrol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelConrol : MonoBehaviour
+{
+    public GameObject start;
+    public GameObject end;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PanelEnd()
+    {
+        start.SetActive(false);
+        end.SetActive(true);
+    }
+
+    public void PanelStart()
+    {
+        start.SetActive(true);
+        end.SetActive(false);
+    }
+
+}
