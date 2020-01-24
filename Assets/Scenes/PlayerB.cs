bb0ef2b87a65e2ea29c926c2b16265ea512d4e30
PodoletsN <58085495+PodoletsN@users.noreply.github.com>
2020-01-24 08:21:42 +0200

    Moved

--- Assets/Scenes/PlayerB.cs
+++ Assets/Scenes/PlayerB.cs
@@ -2,38 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayersMove : MonoBehaviour
+public class PlayerB : MonoBehaviour
 {
-
-    public RouteG currentRoute;
+    public RouteB currentRoute;
 
     int routePosition;
 
     public int steps;
 
-    public int i;
-
     bool isMoving;
-
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space) && !isMoving)
         {
-            steps = DiceNumberTextScript.diceNumber;
-            Debug.Log(" Dice Rolled " + steps);
+            steps = Random.Range(1, 7);
+            Debug.Log("Dice Rolled " + steps);
 
             StartCoroutine(Move());
-            //if(routePosition + steps< currentRoute.childNodeList.Count)
-            //{
-            //    StartCoroutine(Move());
-            //}
-            //else
-            //{
-            //    Debug.Log(" Rolled Number Is To High");
-            //}
         }
     }
-
     IEnumerator Move()
     {
         if (isMoving)
@@ -46,19 +33,15 @@ public class PlayersMove : MonoBehaviour
         {
             routePosition++;
             routePosition %= currentRoute.childNodeList.Count;
-
             Vector3 nextPos = currentRoute.childNodeList[routePosition].position;
-            while (MoveToNextNode(nextPos))
-            {
-                yield return null;
-            }
+            while (MoveToNextNode(nextPos)) { yield return null; }
 
             yield return new WaitForSeconds(0.1f);
             steps--;
             //routePosition++;
         }
-        isMoving = false;
 
+        isMoving = false;
     }
 
     bool MoveToNextNode(Vector3 goal)
